2f14cede1fc8729bad1072d35c2cee7d3fbd6202
Filip Kniaś <filip.knias@o2.pl>
2025-04-06 10:44:56 +0200

    setup index page

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,26 @@ const app = express();
 
 app.use(express.json());
 app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
 connectDB();
 
-app.use('/', (req, res) => {
-  res.render('index.ejs');
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/submit', (req, res) => {
+  const { email, credit } = req.body;
+  try {
+    // const submission = new Submission({ email, credit });
+    // await submission.save();
+    res.redirect(`/summary?email=${email}&credit=${credit}`);
+  } catch (err) {
+    res.status(500).send('Error saving data');
+  }
 });
 
 export default app;
